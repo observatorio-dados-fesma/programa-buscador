369f7242e955db3c570397b28b19ad034a3b77c5
Tiago Matos <69057723+tiagolofi@users.noreply.github.com>
2023-08-21 15:55:13 -0300

    Add files via upload

--- buscador.py
+++ buscador.py
@@ -2,14 +2,12 @@
 from robo2 import Robo
 import PySimpleGUI as sg
 
-sg.theme('DarkBlack')
+robo = Robo()
 
 layout = [
 	[sg.Text('Buscador de Processos', font = ('Lato', 20))],
-	[sg.Text('Credenciais do Eprocessos')], 
-	[sg.InputText('CPF', key = '-USER-'), sg.InputText('Senha', key = '-PWD-')],
-	[sg.InputText('Caminho do arquivo', key = '-FILE-'), sg.FileBrowse('Buscar')],
-	[sg.Button('Iniciar Consulta'), sg.Button('Testar Consulta')]
+	[sg.InputText(key = '-FILE-'), sg.FileBrowse()],
+	[sg.Button('Buscar')]
 ]
 
 window = sg.Window('Buscador de Processos', layout)
@@ -18,17 +16,13 @@ while True:
 
 	event, values = window.read()
 
-	robo = Robo(cpf = values['-USER-'], senha = values['-PWD-'])
+	# arquivo = robo.read_path(values['-FILE-'])
 
-	if event == 'Iniciar':
+	if event == 'Buscar':
 
 		arquivo = robo.read_path(values['-FILE-'])
 
 		robo.buscar(lista = arquivo)
-
-	if event == 'Testar Consulta':
-
-		robo.buscar(lista = robo.read_path('processos.txt'))
 
 	elif event == sg.WINDOW_CLOSED:
 
